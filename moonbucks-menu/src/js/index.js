192231f5904f1bb7982cab769f7567f85e96a3ac
seunghoria <102285042+seunghoria@users.noreply.github.com>
2022-07-29 15:52:42 +0900

    step3 _ MenuApi 객체 분리 _ 메뉴 삭제

--- moonbucks-menu/src/js/index.js
+++ moonbucks-menu/src/js/index.js
@@ -6,7 +6,7 @@
 //- [x] 서버에 카테고리별 메뉴리스트를 불러온다.
 //- [x] 서버에 메뉴가 수정될 수 있도록 요청한다.
 //- [x] 서버에 메뉴의 품절상태를 토글될 수 있도록 요청한다.
-//- [] 서버에 메뉴가 삭제될 수 있도록 요청한다.
+//- [x] 서버에 메뉴가 삭제될 수 있도록 요청한다.
 
 //TODO 리팩터링 부분
 //- [x] localStorage에 저장하는 로직은 지운다.
@@ -57,7 +57,18 @@ const MenuApi = {
     const response = await fetch(
       `${BASE_URL}/category/${category}/menu/${menuId}/soldout`,
       {
-        method: "PUT"
+        method: "PUT",
+      }
+    );
+    if (!response.ok) {
+      console.error("에러가 발생했습니다.");
+    }
+  },
+  async deleteMenu (category, menuId) {
+    const response = await fetch(
+      `${BASE_URL}/category/${category}/menu/${menuId}`,
+      {
+        method: "DELETE",
       }
     );
     if (!response.ok) {
@@ -156,11 +167,13 @@ function App() {
     render();
   };
 
-  const removeMenuName = (e) => {
+  const removeMenuName = async (e) => {
     if(confirm("정말 삭제하시겠습니까?")) {
       const menuId = e.target.closest("li").dataset.menuId;
-      this.menu[this.currentCategory].splice(menuId, 1);
-      store.setLocalStorage(this.menu);
+      await MenuApi.deleteMenu(this.currentCategory, menuId);
+      this.menu[this.currentCategory] = await MenuApi.getAllMenuByCategory(
+        this.currentCategory
+      );
       render();
     }
   };
